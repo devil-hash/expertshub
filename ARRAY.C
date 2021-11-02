27e8f0a4a174da164e8ef17e1d23891df5302efc
Gowtham <63589598+devil-hash@users.noreply.github.com>
2021-11-02 10:56:11 +0530

    Add files via upload

---
+++ ARRAY.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+int array[5]={1,2,3,4,5};
+int y=0;
+int i;
+
+ void main()
+
+ {
+    clrscr();
+    for(y=0;y<=4;y++)
+    {
+    printf("enter the value for a[%d] :",y);
+    scanf("%d",&array[y]);
+    }
+    for (i
+    =0;i<=4;i++)
+    {
+    printf("a[%d] : %d \n",i,array[i]);
+    }
+    getch();
+
+
+
+ }
